8c4e7b0da868c68de0cad0c94ac61437c4fab128
jesterr <deeprocker85@gmail.com>
2022-05-16 17:29:20 +0400

    Task with cycle is ready and works

---
+++ gb/seminar-04-dz/zadacha25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать цикл: принимает 2 числа. Число А возводится в степень В.
+// A = 3 B = 5 -> 243 (3 в 5 степени)
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("В какую степень возвести число? ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int baseNumber = numberA;
+
+for (int i = 1; i != numberB; i++)
+{
+    numberA *= baseNumber;
+}
+
+Console.WriteLine($"Число {baseNumber}, возведенное в степень {numberB} равно: {numberA}.");
